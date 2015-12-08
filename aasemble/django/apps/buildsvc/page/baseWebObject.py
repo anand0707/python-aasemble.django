fb3396260bd016c9ce003a53074a8fee3c37e19d
Anand Bajpai <anand.bajpai@ril.com>
2015-12-08 11:48:26 +0530

    test

--- aasemble/django/apps/buildsvc/page/baseWebObject.py
+++ aasemble/django/apps/buildsvc/page/baseWebObject.py
@@ -27,7 +27,7 @@ class PageObject(StaticLiveServerTestCase):
     def create_login_session(self, username):
         session_cookie = create_session_for_given_user(username)
         print "\n###############\n"
-        print self.live_server_url
+        print dir(self.live_server_url)
         print "\n###############\n"
         self.driver.get(self.live_server_url)
         self.driver.add_cookie(session_cookie)
