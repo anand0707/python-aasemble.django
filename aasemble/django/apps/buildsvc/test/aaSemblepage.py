96a8380815aa92c9f14a36021040baa2e15acc23
Anand Bajpai <anand.bajpai@ril.com>
2015-12-21 12:02:17 +0530

    debug

--- aasemble/django/apps/buildsvc/test/aaSemblepage.py
+++ aasemble/django/apps/buildsvc/test/aaSemblepage.py
@@ -267,7 +267,7 @@ class SnapshotPage(BasePage):
         for ele in elements:
             if ele.find_element(by.By.XPATH, '//td[3]').text == snapshotuuid:
                 ele.find_element(by.By.XPATH, '//td[6]').click()
-                print self.driver.page_source
+                print ele.get_attribute('outerHTML')
                 ele.find_element(by.By.XPATH, '//div[@class="form-group"]').send_keys(tag)
                 self.new_submit_button.click()
 
