890e7cbc056aeb6f98925f9d441d2cb9d34a53ea
Anand Bajpai <anand.bajpai@ril.com>
2015-12-21 13:27:51 +0530

    debug

--- aasemble/django/apps/buildsvc/test/aaSemblepage.py
+++ aasemble/django/apps/buildsvc/test/aaSemblepage.py
@@ -281,8 +281,7 @@ class SnapshotPage(BasePage):
         snaptags = self.getAllTagsBySnapshot(snapshotuuid)
         for snaptag in snaptags:
             if oldtag == snaptag.text:
-                print snaptag.get_attribute('outerHTML')
-                snaptag.click()
+                snaptag.find_element(by.By.XPATH, "//a[contains(text(), %s)]" %(oldtag)).click()
                 self.driver.find_element(by.By.ID, 'id_tag').clear()
                 self.driver.find_element(by.By.ID, 'id_tag').send_keys(tag)
                 self.new_submit_button.click()
