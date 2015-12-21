7e28b5b36e6ef8dbcf01fcaffb69abed6d1a3c77
Anand Bajpai <anand.bajpai@ril.com>
2015-12-21 11:31:05 +0530

    debug

--- aasemble/django/apps/buildsvc/test/aaSemblepage.py
+++ aasemble/django/apps/buildsvc/test/aaSemblepage.py
@@ -254,6 +254,7 @@ class MirrorSetPage(BasePage):
         print mirrorLink.is_enabled()
         print "\n###########################################\n"
         mirrorLink.click()
+        print "\n"
         options = self.driver.find_element(by.By.ID, 'id_mirrors')
         for option in options.find_elements(by.By.TAG_NAME, 'option'):
             option.click()
